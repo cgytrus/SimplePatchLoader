daf917d02683ea9447b84fdedc902cd49831740d
ConfiG <cgytrus@cgyt.ru>
2023-09-12 11:47:40 +0300

    call this aTexCoords instead

--- resources/shaders/pp-vert.glsl
+++ resources/shaders/pp-vert.glsl
@@ -2,11 +2,11 @@
 #extension GL_ARB_explicit_attrib_location : enable
 
 in layout(location = 0) vec2 aPosition;
-in layout(location = 1) vec2 aTexCoord;
+in layout(location = 1) vec2 aTexCoords;
 
 out vec2 TexCoords;
 
 void main() {
     gl_Position = vec4(aPosition.x, aPosition.y, 0.0, 1.0);
-    TexCoords = aTexCoord;
+    TexCoords = aTexCoords;
 }
